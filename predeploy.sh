8fcffa31b6d570f39d1c60e8f75260a08ca167f7
Romain ACCIARI <romain.acciari@gmail.com>
2015-07-17 19:11:37 +0200

    Fix basearch var

--- predeploy.sh
+++ predeploy.sh
@@ -2,8 +2,8 @@
 (
 cat <<EOF
 [openio-sds-unstable-common]
-name=OpenIO SDS unstable packages for Enterprise Linux 7 - $basearch
-baseurl=http://mirror.openio.io/pub/repo/unstable/openio/sds/centos/7/common/$basearch
+name=OpenIO SDS unstable packages for Enterprise Linux 7 - x86_64
+baseurl=http://mirror.openio.io/pub/repo/unstable/openio/sds/centos/7/common/x86_64
 enabled=1
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-OPENIO-0
