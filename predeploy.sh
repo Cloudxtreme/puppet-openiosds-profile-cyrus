df9fe1256e76391e8f1c4aacf95118bfb05902ad
Romain ACCIARI <romain.acciari@gmail.com>
2015-07-17 18:46:42 +0200

    Initial release

---
+++ predeploy.sh
@@ -0,0 +1,13 @@
+# Install unstable repository
+(
+cat <<EOF
+[openio-sds-unstable-common]
+name=OpenIO SDS unstable packages for Enterprise Linux 7 - $basearch
+baseurl=http://mirror.openio.io/pub/repo/unstable/openio/sds/centos/7/common/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-OPENIO-0
+EOF
+) >/etc/yum.repos.d/openio-sds-unstable.repo
+yum clean all
+yum install openio-sds-mod-httpd-test201507171422.master openio-sds-mod-snmp-test201507171422.master openio-sds-common-test201507171422.master openio-sds-server-test201507171422.master
