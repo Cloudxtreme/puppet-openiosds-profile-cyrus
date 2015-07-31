53a47380b19bc6c364d6203423591475ae649622
Romain ACCIARI <romain.acciari@gmail.com>
2015-07-31 12:08:47 +0200

    Fix puppet definitions with latest openiosds module

--- manifests/account-1.pp
+++
@@ -1,7 +0,0 @@
-openiosds::account {'account-1':
-  num => '1',
-  ns => 'OPENIO',
-  redis_default_install => true,
-  redis_host => '127.0.0.1',
-  redis_port => '6379',
-}
---
+++ manifests/cyrus.pp
@@ -0,0 +1,44 @@
+openiosds::sdsagent {'sds-agent-0':
+}
+openiosds::namespace {'OPENIO':
+  ns => 'OPENIO',
+  conscience_url => "${ipaddress}:6000",
+  oioproxy_url   => "${ipaddress}:6006",
+  eventagent_url => "tcp://${ipaddress}:6008",
+}
+openiosds::account {'account-1':
+  num => '1',
+  ns => 'OPENIO',
+  redis_default_install => true,
+  redis_host => '127.0.0.1',
+  redis_port => '6379',
+}
+openiosds::conscience {'conscience-1':
+  num            => '1',
+  ns             => 'OPENIO',
+}
+openiosds::meta0 {'meta0-1':
+  num => '1',
+  ns => 'OPENIO',
+}
+openiosds::meta1 {'meta1-1':
+  num => '1',
+  ns => 'OPENIO',
+}
+openiosds::meta2 {'meta2-1':
+  num => '1',
+  ns => 'OPENIO',
+}
+openiosds::oioeventagent {'oio-event-agent-1':
+  num => '1',
+  ns => 'OPENIO',
+}
+openiosds::oioproxy {'oioproxy-1':
+  num => '1',
+  ns => 'OPENIO',
+  ipaddress => '0.0.0.0',
+}
+openiosds::rawx {'rawx-1':
+  num => '1',
+  ns => 'OPENIO',
+}
